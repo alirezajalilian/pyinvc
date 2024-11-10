445262bdf68be89f7d16cb885645f47d735d9024
a.charehei <a.charehei@limoo.host>
2024-11-10 15:42:21 +0330

    create request module to separate functionality

--- pyinvc/billing.py
+++ pyinvc/billing.py
@@ -1,5 +1,6 @@
 import asyncio
 from datetime import timedelta, datetime
+from enum import Enum
 from typing import List, Dict
 
 import httpx
@@ -7,6 +8,13 @@ from decouple import config
 
 
 class Billing:
+    
+    class RequestMethod(Enum):
+        GET = 1
+        POST = 2
+        DELETE = 3 
+        PATCH = 4
+        PUT = 5
 
     def __init__(self, *, base_url: str = None, secret_token: str = None, user_id: int, filters: str = None):
         self.BASE_URL = base_url or config("BILLING_BASE_URL")
@@ -21,111 +29,115 @@ class Billing:
             raise ValueError("The filters must be string type.")
 
         self.filters = f"&{filters}" if filters else ""
-
-    async def invoice_list_async(self) -> Dict:
+        
+    async def get(self, url: str, data: dict = None):
         async with httpx.AsyncClient() as client:
             response = await client.get(
-                url=f"{self.BASE_URL}/invoice?filters[business_user_id][$eq]={self.user_id}{self.filters}",
+                url=url,
                 headers=self.HEADER,
+                params=data
             )
             response.raise_for_status()
             return response.json()
+    
+    async def post(self, url: str, data: dict = None):
+        async with httpx.AsyncClient() as client:
+            response = await client.post(
+                url=url,
+                headers=self.HEADER,
+                json=data
+            )
+            response.raise_for_status()
+            return response.json()
+        
+    async def request(self, url: str, method: int, data: dict):
+        match method:
+            case Billing.RequestMethod.GET.value:
+                return await self.get(url, data)
+            
+            case Billing.RequestMethod.POST.value:
+                return await self.post(url, data)
+
+    async def invoice_list_async(self) -> Dict:
+        return self.request(
+            method=Billing.RequestMethod.GET.value,
+            url=f"{self.BASE_URL}/invoice?filters[business_user_id][$eq]={self.user_id}{self.filters}",
+        )
 
     def invoice_list_sync(self) -> Dict:
         return asyncio.run(self.invoice_list_async())
 
     async def invoice_detail_async(self, *, invoice_id: int) -> Dict:
-        async with httpx.AsyncClient() as client:
-            response = await client.get(
-                url=f"{self.BASE_URL}/invoice/{invoice_id}?filters[business_user_id][$eq]={self.user_id}{self.filters}",
-                headers=self.HEADER,
-            )
-            response.raise_for_status()
-            return response.json()
+        return await self.request(
+            method=Billing.RequestMethod.GET.value,
+            url=f"{self.BASE_URL}/invoice/{invoice_id}?filters[business_user_id][$eq]={self.user_id}{self.filters}",
+        )
 
     def invoice_detail_sync(self, *, invoice_id: int) -> Dict:
         return asyncio.run(self.invoice_detail_async(invoice_id=invoice_id))
 
     async def invoice_create_async(self, *, items: List[Dict]) -> Dict:
-        async with httpx.AsyncClient() as client:
-            response = await client.post(
-                url=f"{self.BASE_URL}/invoice",
-                headers=self.HEADER,
-                json={
-                    "user_id": self.user_id,
-                    "duedate": (datetime.now() + timedelta(days=3)).strftime("%Y-%m-%dT%H:%M:%S"),
-                    "items": items
-                }
-            )
-            response.raise_for_status()
-            return response.json()
+        return await self.request(
+            method=Billing.RequestMethod.POST.value,
+            url=f"{self.BASE_URL}/invoice",
+            data={
+                "user_id": self.user_id,
+                "duedate": (datetime.now() + timedelta(days=3)).strftime("%Y-%m-%dT%H:%M:%S"),
+                "items": items
+            }
+        )
 
     def invoice_create_sync(self, *, items: List[Dict]) -> Dict:
         return asyncio.run(self.invoice_create_async(items=items))
 
     async def add_promotion_async(self, *, invoice_id: int, promotion_data: dict) -> Dict:
-        async with httpx.AsyncClient() as client:
-            response = await client.post(
-                url=f"{self.BASE_URL}/item/{invoice_id}",
-                headers=self.HEADER,
-                json={
-                    "items": [promotion_data]
-                }
-            )
-            response.raise_for_status()
-            return response.json()
+        return await self.request(
+            method=Billing.RequestMethod.POST.value,
+            url=f"{self.BASE_URL}/item/{invoice_id}",
+            data={
+                "items": [promotion_data]
+            }
+        )
 
     def add_promotion_sync(self, *, invoice_id: int, promotion_data: dict) -> Dict:
         return asyncio.run(self.add_promotion_async(invoice_id=invoice_id, promotion_data=promotion_data))
 
     async def payment_async(self, *, invoice_id: int) -> str:
-        async with httpx.AsyncClient() as client:
-            response = await client.post(
-                url=f"{self.BASE_URL}/payment/{invoice_id}",
-                headers=self.HEADER,
-                json={
-                    "callback_url": f"{config("BILLING_CALLBACK_URL")}/{invoice_id}",
-                }
-            )
-            response.raise_for_status()
-            return response.text
+        return await self.request(
+            method=Billing.RequestMethod.POST.value,
+            url=f"{self.BASE_URL}/payment/{invoice_id}",
+            data={
+                "callback_url": f"{config("BILLING_CALLBACK_URL")}/{invoice_id}",
+            }
+        )
 
     def payment_sync(self, *, invoice_id: int) -> str:
         return asyncio.run(self.payment_async(invoice_id=invoice_id))
 
     async def invoice_delete_item_async(self, *, invoice_id: int, item_id: int) -> Dict:
-        async with httpx.AsyncClient() as client:
-            response = await client.post(
-                url=f"{self.BASE_URL}/item/{invoice_id}/{item_id}",
-                headers=self.HEADER,
-            )
-            response.raise_for_status()
-            return response.json()
+        return await self.request(
+            method=Billing.RequestMethod.POST.value,
+            url=f"{self.BASE_URL}/item/{invoice_id}/{item_id}"
+        )
 
     def invoice_delete_item_sync(self, *, invoice_id: int, item_id: int) -> Dict:
         return asyncio.run(self.invoice_delete_item_async(invoice_id=invoice_id, item_id=item_id))
 
     async def settle_async(self, *, invoice_id: int) -> Dict:
-        async with httpx.AsyncClient() as client:
-            response = await client.post(
-                url=f"{self.BASE_URL}/invoice/settel",
-                headers=self.HEADER,
-                json={"invoice_id": invoice_id}
-            )
-            response.raise_for_status()
-            return response.json()
+        return await self.request(
+            method=Billing.RequestMethod.POST.value,
+            url=f"{self.BASE_URL}/invoice/settel",
+            data={"invoice_id": invoice_id}
+        )
 
     def settle_sync(self, *, invoice_id: int) -> Dict:
         return asyncio.run(self.settle_async(invoice_id=invoice_id))
 
     async def transactions_async(self, *, invoice_id: int) -> Dict:
-        async with httpx.AsyncClient() as client:
-            response = await client.get(
-                url=f"{self.BASE_URL}/transaction?filters[invoice_id][$eq]={invoice_id}",
-                headers=self.HEADER,
-            )
-            response.raise_for_status()
-            return response.json()
+        return await self.request(
+            method=Billing.RequestMethod.GET.value,
+            url=f"{self.BASE_URL}/transaction?filters[invoice_id][$eq]={invoice_id}"
+        )
 
     def transactions_sync(self, *, invoice_id: int) -> Dict:
         return asyncio.run(self.transactions_async(invoice_id=invoice_id))
