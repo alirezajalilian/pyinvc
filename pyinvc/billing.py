77daaedd0fa0a9325517747bc0f06b9a544c6cd8
m.dehghani <mahdidehghani519@gmail.com>
2025-01-20 15:05:31 +0330

    fix type annotation

--- pyinvc/billing.py
+++ pyinvc/billing.py
@@ -5,6 +5,7 @@ from typing import List, Dict
 
 import httpx
 from decouple import config
+from httpx import Response
 
 
 class Billing:
@@ -55,22 +56,22 @@ class Billing:
             case Billing.RequestMethod.POST.value:
                 return await self.post(url, data)
 
-    async def invoice_list_async(self) -> Dict:
+    async def invoice_list_async(self) -> Response:
         return await self.request(
             method=Billing.RequestMethod.GET.value,
             url=f"{self.BASE_URL}/invoice?filters[business_user_id][$eq]={self.user_id}{self.filters}",
         )
 
-    def invoice_list_sync(self) -> Dict:
+    def invoice_list_sync(self) -> Response:
         return asyncio.run(self.invoice_list_async())
 
-    async def invoice_detail_async(self, *, invoice_id: int) -> Dict:
+    async def invoice_detail_async(self, *, invoice_id: int) -> Response:
         return await self.request(
             method=Billing.RequestMethod.GET.value,
             url=f"{self.BASE_URL}/invoice/{invoice_id}?filters[business_user_id][$eq]={self.user_id}{self.filters}",
         )
 
-    def invoice_detail_sync(self, *, invoice_id: int) -> Dict:
+    def invoice_detail_sync(self, *, invoice_id: int) -> Response:
         return asyncio.run(self.invoice_detail_async(invoice_id=invoice_id))
 
     async def invoice_create_async(self, *, items: List[Dict]) -> Dict:
@@ -84,10 +85,13 @@ class Billing:
             }
         )
 
-    def invoice_create_sync(self, *, items: List[Dict]) -> Dict:
+    def invoice_create_sync(self, *, items: List[Dict], due_date: str = None) -> Response:
+        date_format = "%Y-%m-%dT%H:%M:%S"
+        if due_date and not self._validate_datetime(due_date, date_format):
+            raise ValueError(f"Invalid due_date format: {due_date}. Expected format is '{date_format}'.")
         return asyncio.run(self.invoice_create_async(items=items))
 
-    async def add_promotion_async(self, *, invoice_id: int, promotion_data: dict) -> Dict:
+    async def add_promotion_async(self, *, invoice_id: int, promotion_data: dict) -> Response:
         return await self.request(
             method=Billing.RequestMethod.POST.value,
             url=f"{self.BASE_URL}/item/{invoice_id}",
@@ -96,10 +100,10 @@ class Billing:
             }
         )
 
-    def add_promotion_sync(self, *, invoice_id: int, promotion_data: dict) -> Dict:
+    def add_promotion_sync(self, *, invoice_id: int, promotion_data: dict) -> Response:
         return asyncio.run(self.add_promotion_async(invoice_id=invoice_id, promotion_data=promotion_data))
 
-    async def payment_async(self, *, invoice_id: int, payment_type: str = None) -> str:
+    async def payment_async(self, *, invoice_id: int, payment_type: str = None) -> Response:
         return await self.request(
             method=Billing.RequestMethod.POST.value,
             url=f"{self.BASE_URL}/payment/{invoice_id}",
@@ -109,35 +113,35 @@ class Billing:
             }
         )
 
-    def payment_sync(self, *, invoice_id: int, payment_type: str = None) -> str:
+    def payment_sync(self, *, invoice_id: int, payment_type: str = None) -> Response:
         return asyncio.run(self.payment_async(invoice_id=invoice_id, payment_type=payment_type))
 
-    async def invoice_delete_item_async(self, *, invoice_id: int, item_id: int) -> Dict:
+    async def invoice_delete_item_async(self, *, invoice_id: int, item_id: int) -> Response:
         return await self.request(
             method=Billing.RequestMethod.POST.value,
             url=f"{self.BASE_URL}/item/{invoice_id}/{item_id}"
         )
 
-    def invoice_delete_item_sync(self, *, invoice_id: int, item_id: int) -> Dict:
+    def invoice_delete_item_sync(self, *, invoice_id: int, item_id: int) -> Response:
         return asyncio.run(self.invoice_delete_item_async(invoice_id=invoice_id, item_id=item_id))
 
-    async def settle_async(self, *, invoice_id: int) -> Dict:
+    async def settle_async(self, *, invoice_id: int) -> Response:
         return await self.request(
             method=Billing.RequestMethod.POST.value,
-            url=f"{self.BASE_URL}/invoice/settel",
+            url=f"{self.BASE_URL}/invoice/settel",  # noqa
             data={"invoice_id": invoice_id}
         )
 
-    def settle_sync(self, *, invoice_id: int) -> Dict:
+    def settle_sync(self, *, invoice_id: int) -> Response:
         return asyncio.run(self.settle_async(invoice_id=invoice_id))
 
-    async def transactions_async(self, *, invoice_id: int) -> Dict:
+    async def transactions_async(self, *, invoice_id: int) -> Response:
         return await self.request(
             method=Billing.RequestMethod.GET.value,
             url=f"{self.BASE_URL}/transaction?filters[invoice_id][$eq]={invoice_id}"
         )
 
-    def transactions_sync(self, *, invoice_id: int) -> Dict:
+    def transactions_sync(self, *, invoice_id: int) -> Response:
         return asyncio.run(self.transactions_async(invoice_id=invoice_id))
 
     async def wallet_create_async(self):
@@ -178,15 +182,15 @@ class Billing:
 
     def credit_transaction_create_sync(self, amount: int, type_: str, description: str = ""):
         return asyncio.run(self.credit_transaction_create_async(amount, type_, description))
-    
+
     async def billable_create_async(
-            self, 
-            invoice_item_id: int, 
-            amount: float, 
-            description: str, 
-            started_at: str, 
+            self,
+            invoice_item_id: int,
+            amount: float,
+            description: str,
+            started_at: str,
             ended_at: str
-        ):
+    ):
         """
             URL : https://sample-domain/api/v1/billable
             Method : POST    
@@ -197,7 +201,7 @@ class Billing:
                 - description = the description of billable
                 - started_at = the start date of billable in string format
                 - ended_at = the end date of billable in string format
-        """ 
+        """
 
         return await self.request(
             url=f"{self.BASE_URL}/billable",
@@ -211,21 +215,21 @@ class Billing:
                 "user_id": self.user_id,
             }
         )
-    
+
     def billable_create_sync(
-            self, 
-            invoice_item_id: int, 
-            amount: float, 
-            description: str, 
-            started_at: str, 
+            self,
+            invoice_item_id: int,
+            amount: float,
+            description: str,
+            started_at: str,
             ended_at: str
-        ):
+    ):
         return asyncio.run(
             self.billable_create_async(
-                invoice_item_id, 
-                amount, 
-                description, 
-                started_at, 
+                invoice_item_id,
+                amount,
+                description,
+                started_at,
                 ended_at
             )
         )
@@ -244,10 +248,10 @@ class Billing:
                 "invoice_item_id": invoice_item_id
             }
         )
-    
+
     def billable_pay_sync(self, invoice_item_id: int):
         return asyncio.run(self.billable_pay_async(invoice_item_id))
-    
+
     async def billable_collect_async(self, invoice_item_id: int):
         """ 
             URL : https://sample-domain/api/v1/billable/collect
@@ -262,6 +266,6 @@ class Billing:
                 "invoice_item_id": invoice_item_id
             }
         )
-    
+
     def billable_collect_sync(self, invoice_item_id: int):
         return asyncio.run(self.billable_collect_async(invoice_item_id))
